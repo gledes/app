2fa0c99683cace3e74b2a833709a45f5fc994a1f
gledes <6671955@qq.com>
2018-01-06 17:12:02 +0800

    1

--- app/src/main/java/com/example/jin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jin/myapplication/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends Activity {
         @Override
         public void run() {
             System.out.print("UpdateThread");
-            Log.i("UpdateThread", "UpdateThread");
+            Log.i(tag, "UpdateThread");
             handler.postDelayed(updateThread, 3000);
         }
     };
