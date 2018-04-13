b145e66ae69aa4689350ef770f7d9a7f6a686a59
gledes <6671955@qq.com>
2018-04-13 08:17:02 +0800

    1

--- app/src/main/java/com/example/jin/myapplication/floatwindow/GetFloatWindowDirectlyActivity.java
+++ app/src/main/java/com/example/jin/myapplication/floatwindow/GetFloatWindowDirectlyActivity.java
@@ -1,13 +1,18 @@
 package com.example.jin.myapplication.floatwindow;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.jin.myapplication.R;
@@ -53,7 +58,37 @@ public class GetFloatWindowDirectlyActivity extends Activity {
         }
     }
 
-    public void onClick(View v) {
+    public void OnClick(View v) {
+
+        WindowManager windowManager = (WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Point size = new Point();
+        windowManager.getDefaultDisplay().getSize(size);
+        int screenWidth = size.x;
+        int screenHeight = size.y;
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        params.packageName = getPackageName();
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
+                | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+        params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        params.format = PixelFormat.RGBA_8888;
+        params.gravity = Gravity.LEFT | Gravity.TOP;
+        params.x = screenWidth - dp2px(this, 100);
+        params.y = screenHeight - dp2px(this, 171);;
+        AVCallFloatView floatView = new AVCallFloatView(this);
+
+        floatView.setParams(params);
+        floatView.setIsShowing(true);
+        windowManager.addView(floatView, params);
 
     }
+
+    private int dp2px(Context context, float dp){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+
 }
