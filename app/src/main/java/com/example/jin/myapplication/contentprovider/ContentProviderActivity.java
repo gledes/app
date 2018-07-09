356a5006371dd6b5a2f1c53b6f55ec06548548b4
gledes <6671955@qq.com>
2018-07-09 23:31:32 +0800

    1

--- app/src/main/java/com/example/jin/myapplication/contentprovider/ContentProviderActivity.java
+++ app/src/main/java/com/example/jin/myapplication/contentprovider/ContentProviderActivity.java
@@ -59,4 +59,10 @@ public class ContentProviderActivity extends Activity {
             }
         });
     }
+
+    protected void noteActivity(View v) {
+        Intent intent = new Intent();
+        intent.setClass(this, NoteActivity.class);
+        startActivity(intent);
+    }
 }
