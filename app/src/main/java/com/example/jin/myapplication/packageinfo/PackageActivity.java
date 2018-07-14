c477dd1a0a3ad3b5140ef38b82786fcbdeaa0847
gledes <6671955@qq.com>
2018-07-14 17:11:13 +0800

    1

--- app/src/main/java/com/example/jin/myapplication/packageinfo/PackageActivity.java
+++ app/src/main/java/com/example/jin/myapplication/packageinfo/PackageActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.jin.myapplication.MainActivity;
 import com.example.jin.myapplication.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PackageActivity extends AppCompatActivity {
@@ -23,20 +26,28 @@ public class PackageActivity extends AppCompatActivity {
     }
 
     public void showPackageinfo(View v) {
+
+        List<String> data = new ArrayList<String>();
+
         PackageManager pm = this.getPackageManager();
         List<PackageInfo> list = pm.getInstalledPackages(0);
 
-        StringBuffer sb = new StringBuffer();
+//        StringBuffer sb = new StringBuffer();
         for (PackageInfo info : list) {
             if ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
 //                Log.i(MainActivity.tag, "MainActivity.getAppList, packageInfo=" + info.packageName);
-                sb.append(info.packageName).append("\n");
+//                sb.append(info.packageName).append("\n");
+                data.add(info.packageName);
 
             }
         }
 
-        TextView textView = findViewById(R.id.packageinfo);
-        textView.setText(sb);
+//        TextView textView = findViewById(R.id.packageinfo);
+//        textView.setText(sb);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(PackageActivity.this, android.R.layout.simple_expandable_list_item_1, data);
+        ListView listView = findViewById(R.id.package_list);
+        listView.setAdapter(adapter);
 
     }
 }
