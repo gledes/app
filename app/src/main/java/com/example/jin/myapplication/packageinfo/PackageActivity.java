0601c8b755d05150f0be183b4c9248b19c560bb6
gledes <6671955@qq.com>
2018-07-14 18:08:54 +0800

    1

--- app/src/main/java/com/example/jin/myapplication/packageinfo/PackageActivity.java
+++ app/src/main/java/com/example/jin/myapplication/packageinfo/PackageActivity.java
@@ -1,12 +1,16 @@
 package com.example.jin.myapplication.packageinfo;
 
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,7 +35,6 @@ public class PackageActivity extends AppCompatActivity {
 
         PackageManager pm = this.getPackageManager();
         List<PackageInfo> list = pm.getInstalledPackages(0);
-
 //        StringBuffer sb = new StringBuffer();
         for (PackageInfo info : list) {
             if ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
@@ -48,6 +51,27 @@ public class PackageActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(PackageActivity.this, R.layout.item, data);
         ListView listView = findViewById(R.id.package_list);
         listView.setAdapter(adapter);
+
+    }
+
+    public void showClient(View v) {
+        Intent intent = new Intent();
+        intent.setPackage("com.example.clientapp");
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.MATCH_ALL);
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(PackageActivity.this, R.layout.item, list) {
+            public View getView(int position,  View convertView, ViewGroup parent) {
+
+                TextView view = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+                ResolveInfo item = getItem(position);
+                view.setText(item.activityInfo.name);
+
+                return view;
+            }
+        };
+        ListView listView = findViewById(R.id.package_list);
+        listView.setAdapter(adapter);
+
 
     }
 }
